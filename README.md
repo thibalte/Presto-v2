5bd49ba5fd3c430d601fb3ce9d962fb572171909
thibault <hi@thibault.io>
2015-02-25 22:18:05 +0100

    added GRBL config and edited servo controller firmware

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ The all-new Presto v2 featuring:
 - easy electronics, no need for 2 arduinos
 - host software for auto/manual printing, on-the-fly G-Code generation
 
-## references
+## References
 
 - Main Y tube gear generated with [this tool](http://hessmer.org/gears/InvoluteSpurGearBuilder.html).
 - Improvements on side frames and x-carriage thanks to Matt Maurer, Fabian Ritzi, Kevin Rentz @ Hyperwerk, Basel CH.
--- servo-controller/firmware/servo_controller/servo_controller.ino
+++ servo-controller/firmware/servo_controller/servo_controller.ino
@@ -1,11 +1,8 @@
 //
-//  Servo Controller
-//  Thibault Brevet
-//  
-//  Flash on an ATTiny45/85
+// Presto v2 Servo Controller firmware
+// Thibault Brevet, 2015.
 //
-//  SoftwareServo library required, set the ATTiny to work at 1Mhz
-//
+// flash on ATTiny45/85
 //
 
 #include <SoftwareServo.h>
@@ -14,7 +11,7 @@
 SoftwareServo servo;
 int in = 0;
 int pot = A1;
-int up = 0;
+int up = 0; // servo doesnt like super small value
 
 void setup()
 {
@@ -22,20 +19,26 @@ void setup()
   pinMode(in, INPUT);
   pinMode(pot, INPUT);
   servo.attach(1);
+  // overwrite defaults
+  servo.setMinimumPulse(500);
+  servo.setMaximumPulse(2400);
 }
 
 void loop() {
   // check what setting is with the pot
-  int down = analogRead(pot) / 4;
+  int down = map(analogRead(pot), 0, 1024, 0, 180);
+  int val = digitalRead(in);
+  delay(1);
 
   // if the input is HIGH set the servo down
-  if (digitalRead(in)){
+  if (val){
     // move down
     servo.write(down);
-    SoftwareServo::refresh();
   } else {
     // else move up
     servo.write(up);
-    SoftwareServo::refresh();
   }
+
+  SoftwareServo::refresh();
+  delay(10);
 }
